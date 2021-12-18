c54612d3c99b2258aa0f174f2ff6f43d1e76d351
dn5772 <shendanny@naver.com>
2021-12-18 22:33:22 +0900

    1

--- 20202058.cpp
+++ 20202058.cpp
@@ -4,17 +4,90 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <list>
 
 using namespace std;
+
+vector<vector<double>> vertex;
 
 inline double dist(vector<double> &x, vector<double> &y){
 	return sqrt(pow(x[0] - y[0], 2) + pow(x[1] - y[1], 2));
 }
 
+class Path{
+	private :
+	vector<int> x;
+	double tot_cost;
+	int best_index;
+	double best_cost;
+
+	public :
+	Path();
+	Path(vector<int> a);
+	~Path();
+
+	double getcost();
+	int get_x(int index);
+
+	void cal_cost();
+
+	bool operator> (Path& pa);
+	bool operator>= (Path& pa);
+};
+
+Path::Path(){
+	x.resize(1000);
+	best_index = 0;
+	tot_cost = 0;
+	best_cost - 0;
+}
+
+Path::Path(vector<int> a){
+	x = a;
+	best_index = 0;
+	tot_cost = 0;
+	best_cost = 0;
+	cal_cost();
+}
+
+Path::~Path(){
+	x.~vector();
+}
+
+double Path::getcost(){return tot_cost;}
+
+int Path::get_x(int index){return x[index];}
+
+void Path::cal_cost(){
+	tot_cost = dist(vertex[x[0]], vertex[x[999]]);
+	for (int i=0; i<999; i++){
+		int city_1 = x[i];
+		int city_2 = x[i+1];
+
+		tot_cost += dist(vertex[city_1], vertex[city_2]);
+	}
+}
+
+bool Path::operator> (Path& pa){
+	if (this->tot_cost > pa.tot_cost) {return true;}
+	else {return false;}
+}
+
+bool Path::operator>= (Path& pa){
+	if (this->tot_cost < pa.tot_cost) {return false;}
+	else {return true;}
+}
+
+
+
+
+
+
+
+
 int main(){
 	ifstream data("TSP.csv");
     string line;
-    vector<vector<double>> vertex;
 
     while(getline(data,line)){
         stringstream lineStream(line);
@@ -25,7 +98,6 @@ int main(){
         }
         vertex.push_back(row);
     }
-
 
 	// for (int i=0; i<1000; i++){
 	// 	printf("%.16f %.16f\n", arr[i][0], arr[i][1]);
